c5039261f6f31e994a44cbe19f0cb17402ad675e
Shubhansu kumar <shubhansu2021@gmail.com>
2024-02-20 14:51:49 +0530

    Solution 1: Using Max Heap

--- 00_RopeJoining.cpp
+++ 00_RopeJoining.cpp
@@ -5,12 +5,41 @@
 using namespace std;
 
 // } Driver Code Ends
+class Solution1
+{
+    // Solution Using Max Heap: If we have to use max heap, we can negate all the elements of the given input array.
+    // [3, 4, 5] -> [-3, -4, -5];
+    // Now in max heap -3 would be on top and we can negate after popping.
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        // Max Heap
+        priority_queue<long long> pq;
+
+        long long cost = 0;
+        for(long long i = 0; i < n; ++i) {
+            pq.push((arr[i]) * (-1));
+        }
+        while(pq.size() > 1) {
+            long long first = pq.top();
+            pq.pop();
+            long long second = pq.top();
+            pq.pop();
+            cost += (first + second) * (-1);
+            pq.push((first+second));
+        }
+
+        return cost;
+    }
+};
+
 class Solution
+//  Solution using Min Heap: 
 {
     public:
     //Function to return the minimum cost of connecting the ropes.
     long long minCost(long long arr[], long long n) {
-        // MinHeap
+        // MinHeap: 
         priority_queue<long long, vector<long long>, greater<long long>> pq;
 
         long long cost = 0;
