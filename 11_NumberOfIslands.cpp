b77c016d3b9d47b305808a33510c2ba8aa66db3e
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-12 14:52:53 +0530

    FInd the number of connected islands

---
+++ 11_NumberOfIslands.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/number-of-islands/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+
+
+class Solution {
+    // BFS Solution
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int count = 0;
+
+        vector<int> shiftCol = {0, +1, 0, -1}, shiftRow = {-1, 0, +1, 0};
+
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j){
+                if (grid[i][j] == '1') {
+                    ++count;
+
+                    // Start BFS From (i, j);
+                    int row = i, col = j;
+                    queue<pair<int, int>> q;
+                    q.push({row, col});
+                    grid[row][col] = '2';
+                    while(!q.empty()) {
+                        pair<int, int> p = q.front();
+                        q.pop();
+
+                        row = p.first, col = p.second;
+                        for(int k = 0; k < 4; ++k) {
+                            int newRow = row + shiftRow[k];
+                            int newCol = col + shiftCol[k];
+
+                            if (newRow < 0 || newCol < 0 || newRow >= m || newCol >= n) {
+                                // Invalid index
+                                continue;
+                            }
+
+                            if (grid[newRow][newCol] == '1') {
+                                q.push({newRow, newCol});
+                                grid[newRow][newCol] = '2';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
