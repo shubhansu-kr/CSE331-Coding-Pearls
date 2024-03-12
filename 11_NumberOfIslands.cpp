69f364be633884fbf0189c03549e100a9b8bdd8c
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-12 14:53:15 +0530

    Solution1: Using DFS

--- 11_NumberOfIslands.cpp
+++ 11_NumberOfIslands.cpp
@@ -3,7 +3,51 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // DFS Solution
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int count = 0;
 
+        vector<int> shiftCol = {0, +1, 0, -1}, shiftRow = {-1, 0, +1, 0};
+
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j){
+                if (grid[i][j] == '1') {
+                    ++count;
+
+                    // Start BFS From (i, j);
+                    int row = i, col = j;
+                    stack<pair<int, int>> s;
+                    s.push({row, col});
+                    grid[row][col] = '2';
+                    while(!s.empty()) {
+                        pair<int, int> p = s.top();
+                        s.pop();
+
+                        row = p.first, col = p.second;
+                        for(int k = 0; k < 4; ++k) {
+                            int newRow = row + shiftRow[k];
+                            int newCol = col + shiftCol[k];
+
+                            if (newRow < 0 || newCol < 0 || newRow >= m || newCol >= n) {
+                                // Invalid index
+                                continue;
+                            }
+
+                            if (grid[newRow][newCol] == '1') {
+                                s.push({newRow, newCol});
+                                grid[newRow][newCol] = '2';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
 
 class Solution {
     // BFS Solution
